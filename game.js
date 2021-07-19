e05f9ab4e524d91464ed55ac900024d728a6eb12
Fernando González Brest <fgb@fgbrest.xyz>
2021-07-19 13:27:31 -0300

    if else

--- game.js
+++ game.js
@@ -1,30 +1,72 @@
+// variables
+
 const options = ["rock", "paper", "scissors"]
-0 1 2
+
+let computerSelection
+let playerSelection
+
+// functions
 
 function computerPlay () {
-	// randomly returns Rock, Paper, Scissors
-	// returns computerSelection
-
+	// randomly returns rock, paper, scissors
+	computerSelection = options[Math.floor(Math.random()*options.length)];
+	return computerSelection;
 };
 
-let playerSelection = window.prompt("Choose rock, paper or scissors");
+
+function humanPlay () {
+	playerSelection = window.prompt("Choose rock, paper or scissors");
+	return playerSelection.toLowerCase();
+}
+
 
 function gameRound(playerSelection, computerSelection) {
-	// play a single round of the game
-	playerSelection = playerSelection.toLowerCase();
-	if (computerSelection == playerSelection) {
-		return "It's a tie";
-	};
-	else if (computerSelection == "rock" && playerSelection == "scissors" ||
-		computerSelection == "paper" && playerSelection == "rock" ||
-		computerSelection == "scissors" && playerSelection == "paper")
-	return Computer Wins
+  if (playerSelection === computerSelection) {
+    return "tie";
+  }
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return "player";
+  }
+  if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "rock")
+  ) {
+    return "computer";
+  }
+}
 
-
-
-	if
-};
 
 function game() {
-	#play a 5 round game using gameRound()
+	// play a 5 round game using gameRound()
+	console.log("Round 1")
+	humanPlay();
+	computerPlay();
+	console.log(`Computer: ${computerSelection}. Human: ${playerSelection}`)
+	console.log(gameRound(computerSelection, playerSelection));
+	console.log("Round 2")
+	humanPlay();
+	computerPlay();
+	console.log(`Computer: ${computerSelection}. Human: ${playerSelection}`)
+	console.log(gameRound(computerSelection, playerSelection));
+	console.log("Round 3")
+	humanPlay();
+	computerPlay();
+	console.log(`Computer: ${computerSelection}. Human: ${playerSelection}`)
+	console.log(gameRound(computerSelection, playerSelection));
+	console.log("Round 4")
+	humanPlay();
+	computerPlay();
+	console.log(`Computer: ${computerSelection}. Human: ${playerSelection}`)
+	console.log(gameRound(computerSelection, playerSelection));
+	console.log("Round 5")
+	humanPlay();
+	computerPlay();
+	console.log(`Computer: ${computerSelection}. Human: ${playerSelection}`)
+	console.log(gameRound(computerSelection, playerSelection));
+	return "Game finished"
 }
