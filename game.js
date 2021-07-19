5be5f41bab98036b7e930fb32892c58f2ecb97a8
Fernando González Brest <fgb@fgbrest.xyz>
2021-07-18 21:53:56 -0300

    first comm

---
+++ game.js
@@ -0,0 +1,30 @@
+const options = ["rock", "paper", "scissors"]
+0 1 2
+
+function computerPlay () {
+	// randomly returns Rock, Paper, Scissors
+	// returns computerSelection
+
+};
+
+let playerSelection = window.prompt("Choose rock, paper or scissors");
+
+function gameRound(playerSelection, computerSelection) {
+	// play a single round of the game
+	playerSelection = playerSelection.toLowerCase();
+	if (computerSelection == playerSelection) {
+		return "It's a tie";
+	};
+	else if (computerSelection == "rock" && playerSelection == "scissors" ||
+		computerSelection == "paper" && playerSelection == "rock" ||
+		computerSelection == "scissors" && playerSelection == "paper")
+	return Computer Wins
+
+
+
+	if
+};
+
+function game() {
+	#play a 5 round game using gameRound()
+}
